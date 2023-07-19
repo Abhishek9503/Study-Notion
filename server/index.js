f6d630e5d6918cd95e7ef1ce2df5078458f159d3
Abhishek <yadavabhishek9503@gmail.com>
2023-07-19 14:15:41 +0530

    cors edit

--- server/index.js
+++ server/index.js
@@ -25,10 +25,7 @@ database.connect();
 app.use(express.json());
 app.use(cookieParser());
 app.use(
-    cors({
-        origin:"http://localhost:3000",
-        credentials:true,
-    })
+    cors()
 )
 
 
