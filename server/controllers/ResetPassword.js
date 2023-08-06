9761209367343d15446b6c9436be53ecce28da54
Abhishek <yadavabhishek9503@gmail.com>
2023-08-06 16:39:43 +0530

    restPassword Url Updated

--- server/controllers/ResetPassword.js
+++ server/controllers/ResetPassword.js
@@ -25,7 +25,9 @@ exports.resetPasswordToken = async (req, res) => {
 		);
 		console.log("DETAILS", updatedDetails);
 
-		const url = `http://localhost:3000/update-password/${token}`;
+		// const url = `http://localhost:3000/update-password/${token}`;
+		const url = `https://studynotion-edtech-project.vercel.app/update-password/${token}`
+
 
 		await mailSender(
 			email,
